5bc1522310c40e7ca76ed1b381a1e9d71fa34cec
Shuyen Lo <sycw723@gmail.com>
2024-07-17 10:31:30 +0800

    Modify order of county response

--- app/routers/weather.py
+++ app/routers/weather.py
@@ -11,6 +11,8 @@ data2 = None
 data1_active = False
 data2_active = False
 
+county_list = ["基隆市","臺北市","新北市","桃園市","新竹市","新竹縣","苗栗縣","臺中市","彰化縣","南投縣","雲林縣","嘉義市","嘉義縣","臺南市","高雄市","屏東縣","宜蘭縣","花蓮縣","臺東縣","澎湖縣","金門縣","連江縣"]
+
 def fetchWeatherAll():
     query_param = {"Authorization": "CWA-88EDF13E-87C9-4B54-B0A1-699275CFD8C2"}
     url = "https://opendata.cwa.gov.tw/api/v1/rest/datastore/F-C0032-001"
@@ -55,17 +57,19 @@ def get_all_current_weather() -> CurrentWeather:
         data = data1 if data1_active else data2
         result = []
         publish_time = generate_publish_time(data[0]["weatherElement"][0]["time"][0]["startTime"])
-        for i in data:
-            result.append(
-                {
-                    "county": i["locationName"],
-                    "Wx": i["weatherElement"][0]["time"][0]["parameter"]["parameterName"],
-                    "PoP": i["weatherElement"][1]["time"][0]["parameter"]["parameterName"],
-                    "MinT": i["weatherElement"][2]["time"][0]["parameter"]["parameterName"],
-                    "CI": i["weatherElement"][3]["time"][0]["parameter"]["parameterName"],
-                    "MaxT": i["weatherElement"][4]["time"][0]["parameter"]["parameterName"],
-                }
-            )
+        for index in range(len(county_list)):
+            for i in data:
+                if county_list[index] == i["locationName"]:
+                    result.append(
+                        {
+                            "county": i["locationName"],
+                            "Wx": i["weatherElement"][0]["time"][0]["parameter"]["parameterName"],
+                            "PoP": i["weatherElement"][1]["time"][0]["parameter"]["parameterName"],
+                            "MinT": i["weatherElement"][2]["time"][0]["parameter"]["parameterName"],
+                            "CI": i["weatherElement"][3]["time"][0]["parameter"]["parameterName"],
+                            "MaxT": i["weatherElement"][4]["time"][0]["parameter"]["parameterName"],
+                        }
+                    )
         return {"data": result, "publishTime": publish_time}
     except Exception as e:
         return e
